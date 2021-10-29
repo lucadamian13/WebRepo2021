68dc1f3060cc2e5b4bbeb5929df6c14edc6ebcb9
lucadamian13 <luda4196@colorado.edu>
2021-10-29 08:20:13 -0600

    ya

--- midterm/index.js
+++ midterm/index.js
@@ -1,17 +1,18 @@
 let feeling = ["Good", "Bad", "Ugly"];
 let images = ["Good.jpg", "Bad.jpg", "Ugly.jpg"]
 
-
-
+// Display the tab titles
 for (i=0; i<document.getElementsByClassName("feeling").length; i++){
   document.getElementsByClassName("feeling")[i].innerHTML = "<h2>" + feeling[i] + "</h2>";
   document.getElementsByClassName("feeling")[i].addEventListener('click', setActive, false);
   document.getElementsByClassName("feeling")[i].id = feeling[i];
 }
 
+// Default active page
 document.getElementById("Good").classList.toggle('active');
 document.getElementById("image").innerHTML = "<img src='assets/" + images[0] + "' >";
 
+// CLick between tabs, setting each new click as the active tab
 function setActive(e){
   prevfeeling = document.getElementsByClassName("active")[0].id;
   if (prevfeeling != undefined){
@@ -19,20 +20,19 @@ function setActive(e){
     document.getElementById(prevfeeling + "page").style.display = "none";
   }
 
-  if(e.target.tagName == "H2"){
+if(e.target.tagName == "H2"){
     e.target.parentNode.classList.toggle('active');
   }else{
     e.target.classList.toggle('active');
   }
 
-  feelingpick = document.getElementsByClassName('active')[0].id;
+feelingpick = document.getElementsByClassName('active')[0].id;
 
   document.getElementById(feelingpick + "page").style.display = "block";
   document.getElementById('image').innerHTML = "<img src='assets/" + feelingpick + ".jpg' >";
-
 }
 
-This section allows for all  
+// This section makes the button wipe all text areas
 var input = document.querySelector('#clear');
 var goodarea = document.querySelector('#feelingventgood');
 var badarea = document.querySelector('#feelingventbad');
