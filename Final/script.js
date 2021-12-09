3fe7bb5a63ddc2af54f4cea6d1925556a660d61a
lucadamian13 <luda4196@colorado.edu>
2021-12-09 16:19:50 -0700

    m

--- Final/script.js
+++ Final/script.js
@@ -1,14 +1,20 @@
-const imagesArray = ["RandallPark.jpeg", "StevenYeun.jpeg", "SimuLiu.jpeg"];
-let index = 0;
+// const imagesArray = ["RandallPark.jpeg", "StevenYeun.jpeg", "SimuLiu.jpeg"];
+// let index = 0;
+//
+// document.getElementById("buttonImage").addEventListener("click", buttonClicked);
+//
+// function buttonClicked() {
+//   console.log(imagesArray[index]);
+//   document.getElementById("theImage").src = imagesArray[index];
+//   if (index >= 2) {
+//     index = 0;
+//   } else {
+//     index++;
+//   }
+// }
 
-document.getElementById("buttonImage").addEventListener("click", buttonClicked);
+const imagesArray = ["1.jpg"]
 
-function buttonClicked() {
-  console.log(imagesArray[index]);
-  document.getElementById("theImage").src = imagesArray[index];
-  if (index >= 2) {
-    index = 0;
-  } else {
-    index++;
-  }
-}
+document.getElementbyClass("carousel_button_right").addEventListener("click", buttonClicked);
+
+function
