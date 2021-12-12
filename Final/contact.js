c8840fb1968f832899c9d533da087dce0c36f6bc
lucadamian13 <luda4196@colorado.edu>
2021-12-11 18:35:45 -0700

    final final

--- Final/contact.js
+++ Final/contact.js
@@ -1,34 +1,41 @@
 document.getElementById("photo").addEventListener("click",photobuttonClicked);
 
 function photobuttonClicked(){
-  console.log("photo")
+  console.log("photo");
   window.location.href = "photo.html";
 }
 
 document.getElementById("video").addEventListener("click",videobuttonClicked);
 
 function videobuttonClicked(){
-  console.log("video")
+  console.log("video");
   window.location = "video.html";
 }
 
 document.getElementById("about").addEventListener("click",aboutbuttonClicked);
 
 function aboutbuttonClicked(){
-  console.log("about")
+  console.log("about");
   window.location = "about.html";
 }
 
 document.getElementById("contact").addEventListener("click",contactbuttonClicked);
 
 function contactbuttonClicked(){
-  console.log("contact")
+  console.log("contact");
   window.location = "contact.html";
 }
 
 document.getElementById("name-home").addEventListener("click", namehomeClicked);
 
 function namehomeClicked(){
-  console.log("name-home")
+  console.log("name-home");
   window.location = "index.html";
 }
+
+// document.getElementById("insta").addEventListener("click", instaClicked);
+//
+// function instaClicked(){
+//   console.log("insta");
+//   // a.href = "https://www.instagram.com/lucadamian_/" target="_blank";
+// }
